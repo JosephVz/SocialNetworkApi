259c1cd8f5b13bb59d9ae5b3de5ddcb3a9751b95
unknown <vanzandt.school.email@gmail.com>
2024-02-13 14:37:11 -0500

    Updated thoughtcontroller.js

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
-
-
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/SocialNetwork', {
+    
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 
 });
 
